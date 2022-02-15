25406b532d11fc28a29dfb48a9248d67dbeb40f8
keisyarani <99728545+keisyarani@users.noreply.github.com>
2022-02-15 18:19:10 +0700

    ASD Tugas
    G1F021039 _Khalisa Restu Arindra C

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+#include "clock.h"
+using namespace std;
+
+ int main()
+ {
+ 	return 0;
+    clock c1 = MakeClock(2,30,4);
+    clock c2 = MakeClock(6,0,0);
+    clock sisa = MakeClock(1,62,30);
+
+    PrintClock(c1);
+    PrintClock(c2);
+    
+    sisa = AddClock(c1, c2);
+    PrintClock(sisa);
+
+    return 0;
+ }
